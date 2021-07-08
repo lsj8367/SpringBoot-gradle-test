02ca01e178cc3eb4f3de8ea49a22efe021e86339
SeungJae Lee <lsj8367@naver.com>
2021-07-08 23:46:26 +0900

    fix : timeZone 버그 수정

--- src/main/java/com/lsj8367/book/springboot/Application.java
+++ src/main/java/com/lsj8367/book/springboot/Application.java
@@ -3,9 +3,17 @@ package com.lsj8367.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@EnableJpaAuditing //JPA Auditing 활성화
 // JPA metamodel must not be empty! JPA Auditing 같이있어서 에러발생으로 제거
 @SpringBootApplication
 public class Application {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args){ SpringApplication.run(Application.class, args); }
 }
